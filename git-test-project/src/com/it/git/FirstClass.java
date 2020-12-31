55a254bfe8d47938d82de77085707e0bb777851d
zhaopeng <2122352315@qq.com>
2020-12-31 15:09:01 +0800

    提交修改

--- git-test-project/src/com/it/git/FirstClass.java
+++ git-test-project/src/com/it/git/FirstClass.java
@@ -4,6 +4,10 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
     }
 
 
